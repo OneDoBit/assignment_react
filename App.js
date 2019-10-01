076adee007c30dfbccd9781c3571b89e350b9569
OneDoBit <oleksandr.nedobitkov@gmail.com>
2019-09-30 20:56:11 -0400

    first commit
    Signed-off-by: OneDoBit <oleksandr.nedobitkov@gmail.com>

---
+++ App.js
@@ -0,0 +1,70 @@
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator, HeaderTitle } from 'react-navigation-stack';
+import { fromLeft, zoomIn, flipX, flipY, zoomOut, fadeOut } from 'react-navigation-transitions';
+import { Ionicons } from '@expo/vector-icons';
+// import Icon from 'react-native-vector-icons/FontAwesome';
+
+import React, { Component } from 'react';
+
+import HomeScreen from './screens/Home';
+import SettingsScreen from './screens/Settings';
+import LoginScreen from './screens/Login';
+
+// Switch Navigator
+const AuthNavigator = createSwitchNavigator ( 
+  {
+    Login: LoginScreen
+  },
+  {
+    initialRouteName: 'Login'
+  }
+);
+
+// Tab Navigator
+const HomeNavigator = createBottomTabNavigator(
+  {
+    Home: { screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) => <Ionicons name="ios-home" size={32} color={tintColor} />,
+        }, 
+      },
+    About: { screen: SettingsScreen,
+      navigationOptions: {
+        activeTintColor: '#black',
+        tabBarIcon: ({tintColor}) => <Ionicons name="ios-person" size={32} color={tintColor} />
+        },
+      },
+  },
+  {
+    initialRouteName: 'Home'
+  },
+)
+
+// app entry point
+export default createAppContainer( // must have this wrapper (function) to have a starting point for the navigators
+  createStackNavigator(
+    {
+      App: HomeNavigator,
+      Auth: AuthNavigator
+    },
+    {
+      initialRouteName: 'Auth', // which screen / navigations stack to begin with (string name is sensitive)
+      transitionConfig: () => fadeOut(), // screen to screen transitions
+      headerMode: 'none',
+
+      navigationOptions: {
+        headerVisible: false,
+        showIcon: true,
+      },
+
+      defaultNavigationOptions: { // remove swipe back gesture
+        gesturesEnabled: true,
+        showIcon: true 
+      },
+
+      tabBarOptions: {
+      }, 
+    }
+  )
+);
---
+++ screens/Login.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { Image, Button, TextInput, StyleSheet, Text, View, ScrollView, TouchableOpacity } from 'react-native';
+
+export default class LoginScreen extends Component {
+
+  state = { user: '', pass: undefined } // initialize our default state values...try some different default values: '', 3, undefined, null
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <Image style={{width: 300, height: 300}} source={require('../assets/N-Logo.png')}/>
+
+        <TextInput 
+        onChangeText={
+          // update the user states value
+          text => this.setState({user: text})
+        }
+        style={styles.input}
+        placeholder="Enter Username">
+
+        </TextInput>
+
+        <TextInput 
+        onChangeText={
+          text => this.setState({pass: text})
+        } 
+        style={styles.input} 
+        placeholder="Enter Password" 
+        secureTextEntry={true} // hide text
+        ></TextInput>
+
+        <View style={styles.container2}>
+          <Button 
+            onPress={
+              // Navigate to the App Navigation Stack Next
+              () => this.props.navigation.navigate('App')
+            }
+            style={styles.h1} 
+            color="black"
+            title="Enter"
+          />
+        </View>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    marginTop: -50,
+    alignItems: 'center'
+  },
+  container2: {
+    marginTop: 10,
+    width: "40%",
+    borderRadius: 50,
+    overflow: "hidden"
+  },
+  h1: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  input: {
+    backgroundColor: 'black',
+    marginBottom: 20,
+    padding: 10,
+    width: '60%',
+    borderRadius: 50,
+    color: 'white'
+  }
+});
